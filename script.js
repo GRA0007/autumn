b1112389cee8d75851f101c6e816fde831ffa90e
Benjamin Grant <ben@conducthq.com>
2019-07-25 15:18:07 +1000

    Access Harvest API

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ dark_toggle.onclick = function() {
 	}
 	return false;
 };
+
+function getProjects() {
+	var request = new XMLHttpRequest();
+
+	if (!request) {
+		return false;
+	}
+	request.onreadystatechange = function() {
+		if (request.readyState === XMLHttpRequest.DONE) {
+			if (request.status === 200) {
+				alert(request.responseText);
+			} else {
+				alert('There was a problem with the request.');
+			}
+		}
+	};
+	request.open('GET', 'https://api.harvestapp.com/v2/users/2457945/project_assignments');
+	request.setRequestHeader('Harvest-Account-ID', '543778');
+	request.setRequestHeader('User-Agent', 'Harvest API testing for Autumn (ben@conducthq.com)');
+	request.setRequestHeader('Authorization', 'Bearer 1805146.pt.nu22Dg4SH6m-7iLqZ7JVHPhwOp4NAyp5aHLv7B1Kr4odHy0xOdJ2LUu26ZhLilcuFmNcc88oOLxY6JtMSlAQiw');
+	request.send();
+}
